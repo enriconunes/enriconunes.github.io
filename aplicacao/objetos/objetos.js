1c2145a467c742136cee052c7c0da08e9636fec1
enriconunes <enriconunes02@hotmail.com>
2023-10-13 14:39:27 +0100

    Testando github.io

---
+++ aplicacao/objetos/objetos.js
@@ -0,0 +1,372 @@
+class Logo {
+    constructor(logoImg) {
+        this.imagem = logoImg;
+        this.posY = 0
+        this.posX = 0
+        this.largura = 0
+        this.altura = 0
+
+        // if (width < 800) {
+        //     this.posX = width * 0.5 - width * 0.15
+        //     this.largura = width * 0.3
+        //     this.altura = width * 0.15
+        // } else {
+        //     this.posX = width * 0.5 - width * 0.1
+        //     this.largura = width * 0.2
+        //     this.altura = width * 0.1
+        // }
+    }
+
+    desenhar_logo(tabuleiro) {
+        push();
+
+        this.altura = tabuleiro.largura * 0.2
+        this.largura = tabuleiro.largura * 0.4
+        this.posX = width / 2 - this.largura / 2
+        this.posY = height / 2 - tabuleiro.altura / 1.35
+
+        image(this.imagem, this.posX, this.posY, this.largura, this.altura)
+
+        pop();
+    }
+}
+
+class Botao {
+
+    constructor(posX, posY, largura, altura, cor) {
+        this.corPrimaria = cor;
+        this.corVariante = cor;
+        this.corHover = 0;
+        this.posX = posX;
+        this.posY = posY;
+        this.largura = largura;
+        this.altura = altura;
+        this.larguraBorda = 2;
+        this.corBorda = 255;
+    }
+
+    desenhar_botao() {
+
+        push()
+
+        let hoverBotaoConfig = isHover(this.posX - this.largura / 2, this.posX + this.largura / 2, this.posY - this.altura / 2, this.posY + this.altura / 2)
+
+        if (hoverBotaoConfig) {
+            this.corVariante = this.corHover;
+            cursor(HAND);
+        } else {
+            this.corVariante = this.corPrimaria;
+        }
+
+        strokeWeight(this.larguraBorda);
+        stroke(this.corBorda);
+        rectMode(CENTER);
+        fill(this.corVariante);
+        rect(this.posX, this.posY, this.largura, this.altura);
+
+        pop()
+
+    }
+}
+
+class Dado {
+    constructor(imagem) {
+        this.imagem = imagem;
+        this.tamanho = width * 0.08
+        this.posicaoX = 0
+        this.posicaoY = 0
+
+        if (this.tamanho > 65) {
+            this.tamanho = 65
+        }
+    }
+
+    desenhar_dado(larguraTabuleiro, alturaTabuleiro) {
+
+        push()
+
+        this.posicaoX = width / 2 - larguraTabuleiro / 2
+        this.posicaoY = height / 2 + alturaTabuleiro / 2 + 10
+
+        image(this.imagem, this.posicaoX, this.posicaoY, this.tamanho, this.tamanho);
+
+        let dadoHover = isHover(this.posicaoX, this.posicaoX + this.tamanho, this.posicaoY, this.posicaoY + this.tamanho);
+
+        if (dadoHover) {
+            cursor(HAND)
+        }
+
+        pop()
+    }
+
+    girar_dado() {
+        return int(random(1, 7))
+    }
+
+}
+
+class Tabuleiro {
+
+    constructor() {
+        this.cor = "#699669"
+        this.larguraBorda = 2;
+        this.corBorda = 255;
+
+        //Manter a proporçao 16:9
+        this.largura = width * 0.9;
+        this.altura = this.largura * 0.5625;
+
+        //Garantir que a altura nao passe de 70% da tela
+        if (this.altura > height * 0.7) {
+            this.altura = height * 0.7;
+            //Largura = 177% da altura para manter a prop. 16:9
+            this.largura = this.altura + this.altura * 0.77;
+        }
+    }
+
+    desenhar_tabuleiro() {
+        push()
+
+        fill(this.cor);
+        stroke(this.corBorda);
+        strokeWeight(this.larguraBorda);
+        rectMode(CENTER);
+        rect(width / 2, height / 2, this.largura, this.altura)
+
+        pop()
+    }
+
+    isDesafio(casa) {
+        if (casa.isDesafio) {
+            //variavel global para conferir se um desafio está em aberto
+            desafioAberto = true
+        }
+    }
+
+    exibir_desafio(jogador, casa) {
+        push()
+        //desenhar quadro com o desafio
+        fill(12, 54, 32, 230)
+        stroke(255)
+        strokeWeight(this.larguraBorda)
+        rectMode(CENTER)
+        rect(width / 2, height / 2, this.largura * 0.7, this.altura * 0.3)
+
+        //escrever a mensagem
+        let textoDesafio = `Jogador ${jogador.numero} caiu no desafio da casa ${casa.numero}:\n${casa.textoDesafio}`
+        fill(255, 255, 255, 230)
+        strokeWeight(0)
+        textSize(this.largura * 0.025)
+        textAlign(CENTER, CENTER)
+        text(textoDesafio, width / 2, height / 2)
+        pop()
+    }
+
+    exibir_placar(configuracoesJogo) {
+        push()
+
+        rectMode(CENTER)
+        fill("#393993")
+        stroke(255)
+        strokeWeight(2)
+
+        rect(width / 2, height / 2, this.largura * 0.5, this.altura * 0.6)
+
+        //escrever a mensagem
+        fill(255, 255, 255, 230)
+        strokeWeight(0)
+        textAlign(CENTER, CENTER)
+
+        textSize(this.largura * 0.03)
+        text("ORDEM DE CHEGADA:", width / 2, height / 2 - this.largura * 0.05)
+
+        textSize(this.largura * 0.025)
+        for (let n = 0; n < configuracoesJogo.qtdJogadores - 1; n++) {
+            let texto = `${n + 1}º.     Pontuação: ${configuracoesJogo.ordemChegada[n].pontuacao}`
+            text(texto, width / 2, height / 2 + this.largura * 0.055 * n)
+            configuracoesJogo.ordemChegada[n].desenhar_jogador_placar(this, width / 2 - this.largura * 0.065, height / 2 - this.largura * 0.01 + this.largura * 0.055 * n)
+        }
+
+        pop()
+    }
+}
+
+class Casa {
+
+    constructor(larguraTabueleiro, alturaTabuleiro, numero) {
+        this.largura = larguraTabueleiro / 12;
+        this.altura = alturaTabuleiro / 10;
+        this.posicaoX = 0;
+        this.posicaoY = 0;
+        this.larguraBorda = 1;
+        this.corBorda = 255;
+        this.corFundo = "#185c37"
+        this.numero = numero;
+        this.tamanhoFonte = this.largura * 0.3;
+        this.isDesafio = false
+        this.textoDesafio = ""
+        this.desafioDadoMaiorQue = 0
+        this.desafioDadoMenorQue = 0
+        this.desafioVoltarCasasQtd = 0
+    }
+
+    desenhar_casa(posicaoX, posicaoY) {
+        push()
+
+        //Desenhar retangulo
+        fill(this.corFundo);
+        stroke(this.corBorda);
+        strokeWeight(this.larguraBorda);
+        rectMode(CENTER);
+        rect(posicaoX, posicaoY, this.largura, this.altura);
+
+        //Desenhar texto interno
+        strokeWeight(0)
+        fill(255)
+        textAlign(CENTER, CENTER)
+        textSize(this.tamanhoFonte)
+        text(this.numero, posicaoX, posicaoY)
+
+        //Atribuir valores para a posicao da casa com base no tabuleiro
+        this.posicaoX = posicaoX;
+        this.posicaoY = posicaoY;
+
+        pop()
+    }
+
+    resolver_desafio(jogador, valorDado) {
+        if (valorDado > this.desafioDadoMaiorQue && valorDado < this.desafioDadoMenorQue) {
+            desafioAberto = false
+        } else {
+            jogador.posicao -= this.desafioVoltarCasasQtd
+            desafioAberto = false
+        }
+
+        console.log("Valor dado desafio: ", valorDado)
+    }
+}
+
+class Jogador {
+
+    constructor() {
+        this.numero = ""
+        this.cor = ""
+        this.tamanhoBase = ""
+        this.posicao = 0
+        this.posicaoCirculoX = ""
+        this.posicaoCirculoY = ""
+        this.circuloLargura = ""
+        this.pontuacao = 100
+        this.jogouDado = ""
+        this.estado = ""
+    }
+
+    desenhar_jogador(objetoCasa) {
+        push()
+
+        rectMode(CENTER)
+        fill(this.cor)
+        stroke(255)
+        strokeWeight(2)
+
+        //Formato do jogador
+        this.posicaoCirculoX = objetoCasa.posicaoX
+        this.posicaoCirculoY = objetoCasa.posicaoY - objetoCasa.altura * 0.2
+        this.circuloLargura = objetoCasa.altura * 0.4
+        triangle(this.posicaoCirculoX, this.posicaoCirculoY, this.posicaoCirculoX - this.circuloLargura / 2, this.posicaoCirculoY + this.circuloLargura * 1.5, this.posicaoCirculoX + this.circuloLargura / 2, this.posicaoCirculoY + this.circuloLargura * 1.5)
+        ellipse(this.posicaoCirculoX, this.posicaoCirculoY, this.circuloLargura, this.circuloLargura);
+
+        //Texto do numero do jogador
+        textAlign(CENTER, CENTER)
+        fill(255)
+        strokeWeight(0)
+        text(this.numero, this.posicaoCirculoX, this.posicaoCirculoY)
+
+        pop()
+    }
+
+    desenhar_jogador_placar(tabuleiro, posicaoX, posicaoY) {
+        push()
+
+        rectMode(CENTER)
+        fill(this.cor)
+        stroke(255)
+        strokeWeight(2)
+
+        //Formato do jogador
+        this.posicaoCirculoX = posicaoX
+        this.posicaoCirculoY = posicaoY 
+        this.circuloLargura = tabuleiro.altura / 10 * 0.3
+        triangle(this.posicaoCirculoX, this.posicaoCirculoY, this.posicaoCirculoX - this.circuloLargura / 2, this.posicaoCirculoY + this.circuloLargura * 1.5, this.posicaoCirculoX + this.circuloLargura / 2, this.posicaoCirculoY + this.circuloLargura * 1.5)
+        ellipse(this.posicaoCirculoX, this.posicaoCirculoY, this.circuloLargura, this.circuloLargura);
+
+        //Texto do numero do jogador
+        textAlign(CENTER, CENTER)
+        fill(255)
+        strokeWeight(0)
+        textSize(this.circuloLargura*0.8)
+        text(this.numero, this.posicaoCirculoX, this.posicaoCirculoY)
+
+        pop()
+    }
+
+
+    mover_jogador(valorDado) {
+
+        //So move o jogador se nao houver um desafio em aberto
+        if (!desafioAberto) {
+            if (this.posicao + valorDado <= 46) {
+                if (this.posicao == 0) {
+                    if (valorDado == 6) {
+                        this.posicao = 1
+                    }
+                } else {
+                    this.posicao += valorDado
+                } 
+            }
+        }
+
+        if (this.posicao == 46) {
+            this.estado = "chegou"
+        }
+
+        //Decrementa um valor da pontuacao a cada jogada
+        this.pontuacao--
+    }
+
+    exibir_indicador_turno() {
+        //triangulo
+        fill(255)
+        triangle(this.posicaoCirculoX, this.posicaoCirculoY - this.circuloLargura * 0.8, this.posicaoCirculoX - this.circuloLargura / 2, this.posicaoCirculoY - this.circuloLargura * 2, this.posicaoCirculoX + this.circuloLargura / 2, this.posicaoCirculoY - this.circuloLargura * 2)
+        //numero
+        fill(this.cor)
+        strokeWeight(1)
+        stroke(this.cor)
+        textAlign(CENTER, CENTER)
+        text(this.numero, this.posicaoCirculoX, this.posicaoCirculoY - this.circuloLargura * 1.5)
+    }
+
+}
+
+class Jogo {
+
+    constructor() {
+        this.qtdJogadores = "";
+        this.ordemChegada = [];
+    }
+
+    adicionar_jogador_lista_chegada(jogador){
+        if (!this.ordemChegada.includes(jogador)){
+            this.ordemChegada.push(jogador)
+        }
+    }
+
+    confere_terminou() {
+        if (this.ordemChegada.length == this.qtdJogadores-1){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+}
---
+++ aplicacao/sketch.js
@@ -0,0 +1,177 @@
+let logoImg;
+let imagemDadoUm;
+let imagemDadoDois;
+let imagemDadoTres;
+let imagemDadoQuatro;
+let imagemDadoCinco;
+let imagemDadoSeis;
+let imagemDadoGirar;
+let retornoGirardado;
+let definicoesImg;
+let casa = []
+let jogador = []
+let coresJogadores = []
+let turnoJogador;
+let desafioAberto;
+let ativarDadoDesafio;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  //Upload de imagens
+  logoImg = loadImage("./imagens/logo.png")
+  imagemDadoUm = loadImage("./imagens/dado1.png")
+  imagemDadoDois = loadImage("./imagens/dado2.png")
+  imagemDadoTres = loadImage("./imagens/dado3.png")
+  imagemDadoQuatro = loadImage("./imagens/dado4.png")
+  imagemDadoCinco = loadImage("./imagens/dado5.png")
+  imagemDadoSeis = loadImage("./imagens/dado6.png")
+  imagemDadoGirar = loadImage("./imagens/dadoGirar.png")
+  definicoesImg = loadImage("./imagens/definicoes.png")
+
+  //Instanciar objetos
+  logo = new Logo(logoImg);
+  dado = new Dado(imagemDadoSeis);
+  botaoConfig = new Botao(70, 60, 40, 40, "#5858e0");
+  tabuleiro = new Tabuleiro();
+  jogo = new Jogo();
+
+  //Instanciar 46 objetos do tipo Casa
+  for (var i = 0; i < 47; i++) {
+    var casaTemp = new Casa(tabuleiro.largura, tabuleiro.altura);
+    casaTemp.numero = i
+    casa.push(casaTemp);
+  }
+
+  //Definir a quantidade de jogadores
+  jogo.qtdJogadores = 4
+
+  //Instanciar objetos do tipo jogador
+  coresJogadores = ["red", "blue", "purple", "black"]
+  for (var i = 0; i < jogo.qtdJogadores; i++) {
+    var jogadorTemp = new Jogador();
+    jogadorTemp.numero = i + 1
+    jogadorTemp.cor = coresJogadores[i]
+    jogador.push(jogadorTemp);
+  }
+
+  //Configuracoes das casas do tabuleiro
+  configurarCasas()
+
+  //Definir turnos - variavel global
+  turnoJogador = 1;
+
+}
+
+function draw() {
+  cursor(ARROW)
+  //Cor de Fundo
+  background("#5858e0");
+  //Borda
+  createScreenBorder();
+
+  //Desenhar tabuleiro
+  tabuleiro.desenhar_tabuleiro()
+
+  //Desenhar casas do tabeuleiro
+  desenharTodasAsCasas()
+
+  //Se for true, o dado atual é para jogar o desafio - inicia em falso
+  ativarDadoDesafio = false
+
+  //Desenhar jogadores na ordem do ultimo ao primeiro
+  for (let n = jogo.qtdJogadores; n > 0; n--) {
+    jogador[n - 1].desenhar_jogador(casa[jogador[n - 1].posicao])
+  }
+
+  //Se o jogador ja tiver chegado, incrementa um valor e pula vez dele
+  if (jogador[turnoJogador - 1].posicao == 46) {
+    gerirTurno(jogo.qtdJogadores, jogador[turnoJogador - 1])
+  }
+
+  //Desenhar dado
+  switch (retornoGirardado) {
+    case (1):
+      dado.imagem = imagemDadoUm
+      break;
+    case (2):
+      dado.imagem = imagemDadoDois
+      break;
+    case (3):
+      dado.imagem = imagemDadoTres
+      break;
+    case (4):
+      dado.imagem = imagemDadoQuatro
+      break;
+    case (5):
+      dado.imagem = imagemDadoCinco
+      break;
+    case (6):
+      dado.imagem = imagemDadoSeis
+      break;
+  }
+
+  //Botao configuracao
+  botaoConfig.desenhar_botao()
+
+  //Definicoes imagem
+  image(definicoesImg, 55, 45, 30, 30);
+
+  //Mostrar indicador do jogador atual
+  jogador[turnoJogador - 1].exibir_indicador_turno()
+
+  //Exibir caixa de desafio e altera a imagem do dado
+  if (desafioAberto) {
+    tabuleiro.exibir_desafio(jogador[turnoJogador - 1], casa[jogador[turnoJogador - 1].posicao])
+    ativarDadoDesafio = true
+  }
+
+  //Desenhar o dado se o jogo nao tiver terminado
+  if(!jogo.confere_terminou()){
+    dado.desenhar_dado(tabuleiro.largura, tabuleiro.altura);
+  } else{
+    tabuleiro.desenhar_tabuleiro()
+    tabuleiro.exibir_placar(jogo)
+  }
+  
+  //Logo
+  logo.desenhar_logo(tabuleiro);
+
+}
+
+function mousePressed() {
+
+  //Confere se o mouse está em cima do dado e se o jogo ainda nao terminou para executar as ações
+  if (isHover(dado.posicaoX, dado.posicaoX + dado.tamanho, dado.posicaoY, dado.posicaoY + dado.tamanho) && !jogo.confere_terminou()) {
+
+    //Move o jogador e confere se a casa que ele caiu é um desafio
+    retornoGirardado = dado.girar_dado()
+    jogador[turnoJogador - 1].mover_jogador(retornoGirardado)
+    tabuleiro.isDesafio(casa[jogador[turnoJogador - 1].posicao])
+    console.log("Vez do jogador ", turnoJogador, "\nPosicao atual: ", jogador[turnoJogador - 1].posicao, "\nValor dado:    ", retornoGirardado)
+
+    //Adicionar o jogador à lista de jogadores que ja chegaram
+    if (jogador[turnoJogador - 1].posicao == 46) {
+      console.log("O jogador ", jogador[turnoJogador - 1].numero, " chegou!")
+      jogo.adicionar_jogador_lista_chegada(jogador[turnoJogador - 1])
+    }
+
+    //Se for um desafio, nao passa a vez para o proximo jogador - mantem o turno
+    if (!desafioAberto) {      
+      gerirTurno(jogo.qtdJogadores, jogador[turnoJogador - 1])
+    } else {
+
+      //conferir se o dado atual é para o desafio
+      if(ativarDadoDesafio){
+        let retornoGirarDadoDesafio = dado.girar_dado()
+        casa[jogador[turnoJogador - 1].posicao].resolver_desafio(jogador[turnoJogador - 1], retornoGirarDadoDesafio)
+        gerirTurno(jogo.qtdJogadores, jogador[turnoJogador - 1])
+
+        //alterar a imagem do dado
+        retornoGirardado = retornoGirarDadoDesafio
+
+      }
+    }
+  }
+
+}
